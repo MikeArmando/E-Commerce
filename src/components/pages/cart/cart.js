8acfcf67f3a3230b5957999af7fb3bc516655162
Mike Armando <mike.armando.m@gmail.com>
2023-08-14 18:46:32 -0700

    Update

--- src/components/pages/cart/cart.js
+++ src/components/pages/cart/cart.js
@@ -3,25 +3,36 @@ import './cart.css'
 export const Cart = () => {
     return (
         <>
-        <section className='cart-box'>
+            <section className='cart-box'>
 
-            <div className='cart-items-box'>
-                <div className='cart-category'>
-
+                <div className='cart-items-box'>
+                    <table className='cart-category'>
+                        <tr>
+                            <th>Product</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Total</th>
+                        </tr>
+                        <tr>
+                            <td>Name</td>
+                            <td>$49.00</td>
+                            <td>+1-</td>
+                            <td>$49.00</td>
+                        </tr>
+                    </table>
                 </div>
-            </div>
 
-            <div className='cart-info'>
-                <h2>Order Summary</h2>
-                <div>
-                    <p>Shipping</p>
-                    <p>Subtotal</p>
-                    <p>Estimated tax</p>
-                    <p>Order Total</p>
+                <div className='cart-info'>
+                    <h2>Order Summary</h2>
+                    <div>
+                        <p>Shipping</p>
+                        <p>Subtotal</p>
+                        <p>Estimated tax</p>
+                        <p>Order Total</p>
+                    </div>
                 </div>
-            </div>
 
-        </section>
+            </section>
         </>
     )
 }
