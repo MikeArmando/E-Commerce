358f644acc8168c83faf3332439228f76cab95bf
Mike Armando <mike.armando.m@gmail.com>
2023-08-22 18:16:48 -0700

    Update

--- src/components/pages/cart/cart.js
+++ src/components/pages/cart/cart.js
@@ -2,11 +2,18 @@ import './cart.css'
 import { useState } from 'react'
 
 export const Cart = () => {
+    // changes the number of items
     const [countCart, setCountCart] = useState(1);
+    const [countCartS, setCountCartS] = useState(1);
 
     const handleCountCartPlus = () => {
         setCountCart(countCart + 1)
     };
+
+    const handleCountCartPlusSec = () => {
+        setCountCartS(countCartS + 1)
+    };
+
     const handleCountCartMinus = () => {
         setCountCart(countCart - 1)
 
@@ -16,6 +23,18 @@ export const Cart = () => {
 
         } else {
             setCountCart(countCart - 1)
+        }
+    };
+
+    const handleCountCartMinusSec = () => {
+        setCountCartS(countCartS - 1)
+
+        // makes it so count can only go down to 0
+        if (countCartS === 0) {
+            setCountCartS(countCartS)
+
+        } else {
+            setCountCartS(countCartS - 1)
         }
     };
 
@@ -40,7 +59,7 @@ export const Cart = () => {
                         <tr>
                             <td className='img-text'><img src='https://www.cuidadoconelperro.com.mx/media/catalog/product/1/_/1_39948.jpg?width=600&height=800&canvas=600,800&optimize=low&bg-color=255,255,255&fit=bounds  ' alt=''></img><p>Black Cargo Pants</p></td>
                             <td>$49.00</td>
-                            <td className='count-cart-btn'><button onClick={handleCountCartPlus}>+</button>{countCart}<button onClick={handleCountCartMinus}>-</button></td>
+                            <td className='count-cart-btn'><button onClick={handleCountCartPlusSec}>+</button>{countCartS}<button onClick={handleCountCartMinusSec}>-</button></td>
                             <td>$49.00</td>
                         </tr>
                     </table>
