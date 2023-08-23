104d6c5ba350cd04c4868dbe90f19b4f7469a76c
Mike Armando <mike.armando.m@gmail.com>
2023-08-23 15:38:27 -0700

    Update

--- src/components/pages/kids/kids.js
+++ src/components/pages/kids/kids.js
@@ -7,15 +7,53 @@ export const Kids = () => {
             <section className='kids-hero'>
                 <a className='kids-hero-img' href='#!'>
                     <div>
-                        <p className='kids-hero-p1'>Warn Clothes for the weather</p>
+                        <p className='kids-hero-p1'>Warm Clothes for the weather</p>
                         <p>With the most comfortable materials</p>
                         <a href='#!'>Buy Now</a>
                     </div>
                 </a>
             </section>
 
-            <section className='kids-item-box'>
-                
+            <section className='kids-item-center'>
+                <h2>New Arrivals</h2>
+                <div className='kids-item-box'>
+                    <div className="kids-items">
+                        <a href="#!"><img
+                            src="https://www.cuidadoconelperro.com.mx/media/catalog/product/1/_/1_34791.jpg?width=600&height=800&canvas=600,800&optimize=low&bg-color=255,255,255&fit=bounds"
+                            alt=""></img></a>
+                        <div className='kids-text-center'>
+                            <label><a href="#!">Pants</a></label>
+                            <label><a href="#!">$29.99</a></label>
+                        </div>
+                    </div>
+                    <div className="kids-items">
+                        <a href="#!"><img
+                            src="https://www.cuidadoconelperro.com.mx/media/catalog/product/1/_/1_34791.jpg?width=600&height=800&canvas=600,800&optimize=low&bg-color=255,255,255&fit=bounds"
+                            alt=""></img></a>
+                        <div className='kids-text-center'>
+                            <label><a href="#!">Pants</a></label>
+                            <label><a href="#!">$29.99</a></label>
+                        </div>
+                    </div>
+                    <div className="kids-items">
+                        <a href="#!"><img
+                            src="https://www.cuidadoconelperro.com.mx/media/catalog/product/1/_/1_34791.jpg?width=600&height=800&canvas=600,800&optimize=low&bg-color=255,255,255&fit=bounds"
+                            alt=""></img></a>
+                        <div className='kids-text-center'>
+                            <label><a href="#!">Pants</a></label>
+                            <label><a href="#!">$29.99</a></label>
+                        </div>
+                    </div>
+                    <div className="kids-items">
+                        <a href="#!"><img
+                            src="https://www.cuidadoconelperro.com.mx/media/catalog/product/1/_/1_34791.jpg?width=600&height=800&canvas=600,800&optimize=low&bg-color=255,255,255&fit=bounds"
+                            alt=""></img></a>
+                        <div className='kids-text-center'>
+                            <label><a href="#!">Pants</a></label>
+                            <label><a href="#!">$29.99</a></label>
+                        </div>
+                    </div>
+                </div>
             </section>
         </>
     )
