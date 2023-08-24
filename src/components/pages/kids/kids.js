4870b54ef9020c8dbb24b371d3b89d493cd6ae6d
Mike Armando <mike.armando.m@gmail.com>
2023-08-23 19:47:00 -0700

    Update

--- src/components/pages/kids/kids.js
+++ src/components/pages/kids/kids.js
@@ -55,6 +55,75 @@ export const Kids = () => {
                     </div>
                 </div>
             </section>
+
+            <section className="kids-gate-box">
+                <h2>Search By Category</h2>
+                <div className="kids-gate-row">
+
+                    <div className="kid-gate-items">
+                        <a href="#!"><img
+                            src="https://www.cuidadoconelperro.com.mx/media/catalog/product/1/_/1_34857.jpg?width=600&height=800&canvas=600,800&optimize=low&bg-color=255,255,255&fit=bounds"
+                            alt=""></img></a>
+                        <label><a href="#!">Shirts</a></label>
+                        <label><a href="#!">$29.99</a></label>
+                    </div>
+
+                    <div className="kid-gate-items">
+                        <a href="#!"><img
+                            src="https://www.cuidadoconelperro.com.mx/media/catalog/product/1/_/1_34791.jpg?width=600&height=800&canvas=600,800&optimize=low&bg-color=255,255,255&fit=bounds"
+                            alt=""></img></a>
+                        <div className='category-center'>
+                            <label><a href="#!">Pants</a></label>
+                            <label><a href="#!">$29.99</a></label>
+                        </div>
+                    </div>
+                    <div className="kid-gate-items">
+                        <a href="#!"><img
+                            src="https://www.cuidadoconelperro.com.mx/media/catalog/product/1/_/1_31_1.jpg?width=600&height=800&canvas=600,800&optimize=low&bg-color=255,255,255&fit=bounds"
+                            alt=""></img></a>
+                        <div className='category-center'>
+                            <label><a href="#!">jackets</a></label>
+                            <label><a href="#!">$29.99</a></label>
+                        </div>
+                    </div>
+                    <div className="kid-gate-items">
+                        <a href="#!"><img
+                            src="https://www.cuidadoconelperro.com.mx/media/catalog/product/1/_/1_31646.jpg?width=600&height=800&canvas=600,800&optimize=low&bg-color=255,255,255&fit=bounds"
+                            alt=""></img></a>
+                        <div className='category-center'>
+                            <label><a href="#!">sweatshirts</a></label>
+                            <label><a href="#!">$29.99</a></label>
+                        </div>
+                    </div>
+                    <div className="kid-gate-items">
+                        <a href="#!"><img
+                            src="https://www.cuidadoconelperro.com.mx/media/catalog/product/1/_/1_35_3.jpg?width=600&height=800&canvas=600,800&optimize=low&bg-color=255,255,255&fit=bounds"
+                            alt=""></img></a>
+                        <div className='category-center'>
+                            <label><a href="#!">joggers</a></label>
+                            <label><a href="#!">$29.99</a></label>
+                        </div>
+                    </div>
+                    <div className="kid-gate-items">
+                        <a href="#!"><img
+                            src="https://www.cuidadoconelperro.com.mx/media/catalog/product/1/_/1_35454.jpg?width=600&height=800&canvas=600,800&optimize=low&bg-color=255,255,255&fit=bounds"
+                            alt=""></img></a>
+                        <div className='category-center'>
+                            <label><a href="#!">Swimsuits</a></label>
+                            <label><a href="#!">$29.99</a></label>
+                        </div>
+                    </div>
+                    <div className="kid-gate-items">
+                        <a href="#!"><img
+                            src="https://www.cuidadoconelperro.com.mx/media/catalog/product/1/_/1_33059.jpg?width=600&height=800&canvas=600,800&optimize=low&bg-color=255,255,255&fit=bounds"
+                            alt=""></img></a>
+                        <div className='category-center'>
+                            <label><a href="#!">Underwear</a></label>
+                            <label><a href="#!">$29.99</a></label>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
