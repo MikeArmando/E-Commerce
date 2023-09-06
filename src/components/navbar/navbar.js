05c3ebdcd8a0c31f7724b8a49a19dad26718ca04
Mike Armando <mike.armando.m@gmail.com>
2023-09-05 17:38:11 -0700

    Update

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -40,7 +40,7 @@ export const Navbar = () => {
                         <li className="sub-li"><a href="#!">Hoodies</a></li>
                     </ul>
                     <ul className="sub-menu-box sub-menu-box1">
-                        <li className="sub-menu-title"><a href="clothes/clothes.html">Clothes</a></li>
+                        <li className="sub-menu-title"><a href="#!">Clothes</a></li>
                         <li className="sub-li"><a href="#!">T-Shirts</a></li>
                         <li className="sub-li"><a href="#!">Shirts</a></li>
                         <li className="sub-li"><a href="#!">Sweatshirt</a></li>
@@ -50,21 +50,21 @@ export const Navbar = () => {
                         <li className="sub-li"><a href="#!">Swimwear</a></li>
                     </ul>
                     <ul className="sub-menu-box sub-menu-box2">
-                        <li className="sub-menu-title"><a href="jeans/jeans.html">Jeans</a></li>
+                        <li className="sub-menu-title"><a href="#!">Jeans</a></li>
                         <li className="sub-li"><a href="#!">Skinny</a></li>
                         <li className="sub-li"><a href="#!">Relax</a></li>
                         <li className="sub-li"><a href="#!">Slim</a></li>
                         <li className="sub-li"><a href="#!">straight</a></li>
                     </ul>
                     <ul className="sub-menu-box sub-menu-box3">
-                        <li className="sub-menu-title"><a href="underwear/underwear.html">Underwear</a></li>
+                        <li className="sub-menu-title"><a href="#!">Underwear</a></li>
                         <li className="sub-li"><a href="#!">Boxer</a></li>
                         <li className="sub-li"><a href="#!">Socks</a></li>
                     </ul>
                 </li>
                 <li><Link className="le" to="/women" onClick={scrollToTop}>Women</Link>
                     <ul className="sub-menu-box">
-                        <li className="sub-menu-title"><a href="/content/women/collection/collection.html">Collection</a></li>
+                        <li className="sub-menu-title"><a href="#!">Collection</a></li>
                         <li className="sub-li"><a href="#!">Monster High</a></li>
                         <li className="sub-li"><a href="#!">Push Up Jeans</a></li>
                         <li className="sub-li"><a href="#!">Dresses</a></li>
@@ -72,7 +72,7 @@ export const Navbar = () => {
                         <li className="sub-li"><a href="#!">Hoodies</a></li>
                     </ul>
                     <ul className="sub-menu-box sub-menu-box1">
-                        <li className="sub-menu-title"><a href="/content/women/clothes/clothes.html">Clothes</a></li>
+                        <li className="sub-menu-title"><a href="#!">Clothes</a></li>
                         <li className="sub-li"><a href="#!">Tops</a></li>
                         <li className="sub-li"><a href="#!">Blouses</a></li>
                         <li className="sub-li"><a href="#!">Shirts</a></li>
@@ -85,7 +85,7 @@ export const Navbar = () => {
                         <li className="sub-li"><a href="#!">Shorts</a></li>
                     </ul>
                     <ul className="sub-menu-box sub-menu-box2">
-                        <li className="sub-menu-title"><a href="/content/women/jeans/jeans.html">Jeans</a></li>
+                        <li className="sub-menu-title"><a href="#!">Jeans</a></li>
                         <li className="sub-li"><a href="#!">Skinny</a></li>
                         <li className="sub-li"><a href="#!">Wide Leg</a></li>
                         <li className="sub-li"><a href="#!">Flare</a></li>
@@ -95,14 +95,14 @@ export const Navbar = () => {
                         <li className="sub-li"><a href="#!">Straight</a></li>
                     </ul>
                     <ul className="sub-menu-box sub-menu-box3">
-                        <li className="sub-menu-title"><a href="/content/women/sportive/sportive.html">Sportive</a></li>
+                        <li className="sub-menu-title"><a href="#!">Sportive</a></li>
                         <li className="sub-li"><a href="#!">Biker</a></li>
                         <li className="sub-li"><a href="#!">Leggins</a></li>
                         <li className="sub-li"><a href="#!">Short</a></li>
                         <li className="sub-li"><a href="#!">Top</a></li>
                     </ul>
                     <ul className="sub-menu-box sub-menu-box4">
-                        <li className="sub-menu-title"><a href="/content/women/lingerie/lingerie.html">Lingerie</a></li>
+                        <li className="sub-menu-title"><a href="#!">Lingerie</a></li>
                         <li className="sub-li"><a href="#!">Bras</a></li>
                         <li className="sub-li"><a href="#!">Panties</a></li>
                         <li className="sub-li"><a href="#!">Pijamas</a></li>
@@ -161,7 +161,7 @@ export const Navbar = () => {
 
             <div className={`sidebar ${showSidebar ? '' : 'active'}`}>
                 <div className='cart-box'>
-                    <h2>Your Cart</h2>
+                    <h2>Shopping Cart</h2>
                     <div className='cart-content'>
                         <div className='cart-item'>
                             <img src='https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F69%2Fe8%2F69e81c9fc4199eb27b2ac256c439b6fee57916d3.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url[file:/product/fullscreen]' alt=''></img>
