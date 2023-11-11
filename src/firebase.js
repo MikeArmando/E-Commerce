c1dbe1b0d154910bf0c54578396aefb295255610
Mike Armando <mike.armando.m@gmail.com>
2023-11-10 17:41:44 -0800

    Update

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAygCpRCY1ddV3ThuPUgwZZ83HzPxqgZus",
+  authDomain: "fit-and-casual.firebaseapp.com",
+  projectId: "fit-and-casual",
+  storageBucket: "fit-and-casual.appspot.com",
+  messagingSenderId: "98427174910",
+  appId: "1:98427174910:web:8e2ceba8c54322e7448aaf",
+  measurementId: "G-ZGTW71MB7H"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
