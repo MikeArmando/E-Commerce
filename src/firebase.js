2d5b223724dbd14c182d8a3014025f2b91537aa0
Mike Armando <mike.armando.m@gmail.com>
2023-12-18 14:31:13 -0800

    Update

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_PLGZReowbcBjRodsJrNoxZ0cbcl00oc",
-  authDomain: "mike-armando.firebaseapp.com",
-  projectId: "mike-armando",
-  storageBucket: "mike-armando.appspot.com",
-  messagingSenderId: "685339527891",
-  appId: "1:685339527891:web:30ea7f6ac550ea7297e226"
+  apiKey: "AIzaSyDTJfBX7Tik89F8pficaft47ZeqhRm7U4s",
+  authDomain: "mike-montano.firebaseapp.com",
+  projectId: "mike-montano",
+  storageBucket: "mike-montano.appspot.com",
+  messagingSenderId: "699062256141",
+  appId: "1:699062256141:web:cebc4639fca84c97cc8b44"
 };
 
 const app = initializeApp(firebaseConfig);
